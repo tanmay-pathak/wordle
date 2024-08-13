c9baaef253475ae457aff7b89a5a97038093e780
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-08-12 19:12:39 -0600

    chore: logo fix

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -18,12 +18,8 @@ const Appbar = () => {
 		<div className="fixed top-0 left-0 z-30 w-full bg-zinc-900 pt-safe">
 			<header className="border-b bg-zinc-100 px-safe dark:border-zinc-800 dark:bg-zinc-900">
 				<div className="mx-auto flex h-20 max-w-screen-md items-center justify-between px-6">
-					<Link href="/" className="flex gap-1 items-center">
-						<img
-							src="/images/icons/icon-128x128.png"
-							className="size-8 sm:size-12"
-						/>
-						<h1 className="font-medium text-xl sm:text-2xl">dle</h1>
+					<Link href="/">
+						<h1 className="font-medium text-xl sm:text-2xl">tudle</h1>
 					</Link>
 
 					<nav className="flex items-center space-x-6">
